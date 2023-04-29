e649e932aba28a6db5fd10c32e332d1c69ca6410
ramsesar1 <ramsesa_19@alu.uabcs.mx>
2023-04-28 17:44:36 -0700

    Se añaden hitbox a las paredes del laberinto de manera manual con un array, faltan por pulir pero evitan que el jugador se salga

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -7,11 +7,10 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import javax.swing.JButton;
 
 public class Main extends JFrame {
-    JButton jugador = new JButton();
-
     //  int x = 0, y = 0;
 
     private JPanel contentPane;
@@ -49,7 +48,7 @@ public class Main extends JFrame {
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 g.setColor(Color.BLACK);
-                g.fillRect(30, 0, 550, 15);
+             //   g.fillRect(30, 0, 550, 15);
                 g.fillRect(30, 30, 15, 280);
                 g.fillRect(560, 0, 15, 280);
 
@@ -74,20 +73,36 @@ public class Main extends JFrame {
 
                 g.fillRect(30, 300, 550, 15);
 
-                jugador.setLocation(0, 0);
+                g.setColor(Color.RED);
+                g.fillRect(550, 285, 30, 15);
 
                 //columnas
                 // g.fillRect(90, 30, 15, 30);
             }
         };
 
+
+        ArrayList<Rectangle> hitbox = new ArrayList<>();
+        hitbox.add(new Rectangle(30, 30, 10, 280));
+        hitbox.add(new Rectangle(570, 0, 10, 280));
+        hitbox.add(new Rectangle(30, 30, 520, 10));
+        hitbox.add(new Rectangle(60, 60, 550, 10));
+        hitbox.add(new Rectangle(30, 90, 520, 10));
+        hitbox.add(new Rectangle(60, 120, 550, 10));
+        hitbox.add(new Rectangle(30, 150, 520, 10));
+        hitbox.add(new Rectangle(60, 180, 550, 10));
+        hitbox.add(new Rectangle(30, 210, 520, 10));
+        hitbox.add(new Rectangle(30, 240, 520, 10));
+        hitbox.add(new Rectangle(60, 270, 520, 10));
+
+        JButton jugador = new JButton();
         jugador.setBackground(Color.GREEN);
         jugador.setPreferredSize(new Dimension(10, 10));
-
+        jugador.setBounds(45, 45, 10, 10);
         nivel1.add(jugador);
 
         contentPane.add(nivel1, BorderLayout.CENTER);
-        nivel1.setSize(contentPane.getSize());
+
         JPanel panel_1 = new JPanel();
         panel_1.setBackground(new Color(0, 255, 0));
         contentPane.add(panel_1, BorderLayout.SOUTH);
@@ -106,25 +121,64 @@ public class Main extends JFrame {
                 System.out.println("keyPressed");
                 System.out.println(e.getKeyCode());
                 System.out.println(e.getKeyChar());
-
                 if (e.getKeyCode() == KeyEvent.VK_A) {
-                    jugador.setLocation(jugador.getX() - 10, jugador.getY());
-                    repaint();
+                    Rectangle contacto = new Rectangle(jugador.getX() - 10, jugador.getY(), jugador.getWidth(), jugador.getHeight());
+                    boolean puedemoverse = true;
+                    for (Rectangle rect : hitbox) {
+                        if (rect.intersects(contacto)) {
+                            puedemoverse = false;
+                            break;
+                        }
+                    }
+                    if (puedemoverse) {
+                        jugador.setLocation(jugador.getX() - 10, jugador.getY());
+                        repaint();
+                    }
                 }
 
                 if (e.getKeyCode() == KeyEvent.VK_D) {
-                    jugador.setLocation(jugador.getX() + 10, jugador.getY());
-                    repaint();
+                    Rectangle contacto = new Rectangle(jugador.getX() + 10, jugador.getY(), jugador.getWidth(), jugador.getHeight());
+                    boolean puedemoverse = true;
+                    for (Rectangle rect : hitbox) {
+                        if (rect.intersects(contacto)) {
+                            puedemoverse = false;
+                            break;
+                        }
+                    }
+                    if (puedemoverse) {
+                        jugador.setLocation(jugador.getX() + 10, jugador.getY());
+                        repaint();
+                    }
                 }
 
                 if (e.getKeyCode() == KeyEvent.VK_W) {
-                    jugador.setLocation(jugador.getX(), jugador.getY() - 10);
-                    repaint();
+                    Rectangle contacto = new Rectangle(jugador.getX(), jugador.getY() - 10, jugador.getWidth(), jugador.getHeight());
+                    boolean puedemoverse = true;
+                    for (Rectangle rect : hitbox) {
+                        if (rect.intersects(contacto)) {
+                            puedemoverse = false;
+                            break;
+                        }
+                    }
+                    if (puedemoverse) {
+                        jugador.setLocation(jugador.getX(), jugador.getY() - 10);
+                        repaint();
+                    }
                 }
 
                 if (e.getKeyCode() == KeyEvent.VK_S) {
-                    jugador.setLocation(jugador.getX(), jugador.getY() + 10);
-                    repaint();
+                    Rectangle contacto = new Rectangle(jugador.getX(), jugador.getY() + 10, jugador.getWidth(), jugador.getHeight());
+                    boolean puedemoverse = true;
+                    for (Rectangle rect : hitbox) {
+                        if (rect.intersects(contacto)) {
+                            puedemoverse = false;
+                            break;
+                        }
+                    }
+                    if (puedemoverse) {
+                        jugador.setLocation(jugador.getX(), jugador.getY() + 10);
+                        repaint();
+                    }
                 }
             }
 
@@ -137,4 +191,3 @@ public class Main extends JFrame {
 
     }
 }
-
