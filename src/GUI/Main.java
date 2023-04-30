09ed3e8537599bc6e89f6884ae26f315313ceb53
ramsesar1 <ramsesa_19@alu.uabcs.mx>
2023-04-30 14:32:25 -0700

    Se crea el layout y hitboxes del 2do nivel, aun no hacen colision, pero ya estan dentro del codigo

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -51,6 +51,8 @@ public class Main extends JFrame {
         //fila 1
         nivel1Hitboxes.add(new Rectangle(570, 0, 10, 280));
         nivel1Hitboxes.add(new Rectangle(30, 35, 520, 10));
+        nivel2Hitboxes.add(new Rectangle(30, 35, 520, 10));
+
         nivel1Hitboxes.add(new Rectangle(30, 0, 550, 10));
         nivel1Hitboxes.add(new Rectangle(30, 0, 10, 60));
         //fila2
@@ -118,14 +120,158 @@ public class Main extends JFrame {
         nivel1Hitboxes.add(new Rectangle(120,245,10,30));
 
         //-----------------------hitboxes del nivel2----------------------------------------
+        //se agregaran primero a nivel1 para testeo, despues se hace el reemplazo para nivel2
+
+        nivel2Hitboxes.add(new Rectangle(30, 30, 10, 280));
+        nivel2Hitboxes.add(new Rectangle(30, 0, 550, 10));
+        nivel2Hitboxes.add(new Rectangle(30, 0, 10, 60));
+
+        //----------filas------------
+
+        //fila 1
+        nivel2Hitboxes.add(new Rectangle(570, 0, 10, 280));
+        nivel2Hitboxes.add(new Rectangle(30, 35, 520, 10));
+        nivel2Hitboxes.add(new Rectangle(30, 35, 520, 10));
+
+        //fila2
+        nivel2Hitboxes.add(new Rectangle(30, 65, 110, 10));
+        nivel2Hitboxes.add(new Rectangle(170, 65, 110, 10));
+        nivel2Hitboxes.add(new Rectangle(300, 65, 110, 10));
+        nivel2Hitboxes.add(new Rectangle(430, 65, 110, 10));
+
+        //fila3
+        nivel2Hitboxes.add(new Rectangle(30, 95, 110, 10));
+        nivel2Hitboxes.add(new Rectangle(170, 95, 110, 10));
+        nivel2Hitboxes.add(new Rectangle(300, 95, 110, 10));
+        nivel2Hitboxes.add(new Rectangle(430, 95, 110, 10));
+        //fila4
+        nivel2Hitboxes.add(new Rectangle(30, 125, 110, 10));
+        nivel2Hitboxes.add(new Rectangle(170, 125, 110, 10));
+        nivel2Hitboxes.add(new Rectangle(300, 125, 110, 10));
+        nivel2Hitboxes.add(new Rectangle(430, 125, 110, 10));
+        //fila5
+        nivel2Hitboxes.add(new Rectangle(30, 155, 110, 10));
+        nivel2Hitboxes.add(new Rectangle(170, 155, 110, 10));
+        nivel2Hitboxes.add(new Rectangle(300, 155, 110, 10));
+        nivel2Hitboxes.add(new Rectangle(430, 155, 110, 10));
+        //fila6
+        nivel2Hitboxes.add(new Rectangle(30, 185, 110, 10));
+        nivel2Hitboxes.add(new Rectangle(170, 185, 110, 10));
+        nivel2Hitboxes.add(new Rectangle(300, 185, 110, 10));
+        nivel2Hitboxes.add(new Rectangle(430, 185, 110, 10));
+        //fila7
+        nivel2Hitboxes.add(new Rectangle(30, 215, 110, 10));
+        nivel2Hitboxes.add(new Rectangle(170, 215, 110, 10));
+        nivel2Hitboxes.add(new Rectangle(300, 215, 110, 10));
+        nivel2Hitboxes.add(new Rectangle(430, 215, 110, 10));
+        //fila8
+        nivel2Hitboxes.add(new Rectangle(30, 245, 110, 10));
+        nivel2Hitboxes.add(new Rectangle(170, 245, 110, 10));
+        nivel2Hitboxes.add(new Rectangle(300, 245, 110, 10));
+        nivel2Hitboxes.add(new Rectangle(430, 245, 110, 10));
+        //fila9
+        nivel2Hitboxes.add(new Rectangle(30, 270, 110, 10));
+        nivel2Hitboxes.add(new Rectangle(170, 270, 110, 10));
+        nivel2Hitboxes.add(new Rectangle(300, 270, 110, 10));
+        nivel2Hitboxes.add(new Rectangle(430, 270, 110, 10));
+
+        //filas cortas
+
+        //fila 2
+        nivel2Hitboxes.add(new Rectangle(280, 65, 40, 10));
+        nivel2Hitboxes.add(new Rectangle(410, 65, 40, 10));
+
+     //   nivel1Hitboxes.add(new Rectangle(540, 65, 40, 10));
+
+        //fila3
+        nivel2Hitboxes.add(new Rectangle(280, 95, 40, 10));
+
+        nivel2Hitboxes.add(new Rectangle(410, 95, 40, 10));
+
+        nivel2Hitboxes.add(new Rectangle(540, 95, 40, 10));
+
+
+        //fila4
+        nivel2Hitboxes.add(new Rectangle(540, 125, 40, 10));
+
+        //fila5
+        nivel2Hitboxes.add(new Rectangle(540, 155, 40, 10));
+
+        //fila6
+        nivel2Hitboxes.add(new Rectangle(280, 185, 40, 10));
+
+        nivel2Hitboxes.add(new Rectangle(540, 185, 40, 10));
+
+        //fila7
+        nivel2Hitboxes.add(new Rectangle(410, 215, 40, 10));
+
+        nivel2Hitboxes.add(new Rectangle(540, 215, 40, 10));
+
+        //fila8
+        nivel2Hitboxes.add(new Rectangle(280, 245, 40, 10));
+
+        nivel2Hitboxes.add(new Rectangle(540, 245, 40, 10));
+
+        //fila9
+        nivel2Hitboxes.add(new Rectangle(140, 270, 40, 10));
+
+        nivel2Hitboxes.add(new Rectangle(410, 270, 40, 10));
+
+
+
+
+
+
+        //---------columnas---------------
+
+
+        //fila2
+   //     nivel1Hitboxes.add(new Rectangle(170,35,10,30));
+   //     nivel1Hitboxes.add(new Rectangle(300,35,10,30));
+        //fila3
+        nivel2Hitboxes.add(new Rectangle(480,65,10,30));
+        nivel2Hitboxes.add(new Rectangle(80,65,10,30));
+        //fila4
+        nivel2Hitboxes.add(new Rectangle(360,95,10,30));
+        nivel2Hitboxes.add(new Rectangle(190,95,10,30));
+        //fila5
+        nivel2Hitboxes.add(new Rectangle(210,125,10,30));
+        nivel2Hitboxes.add(new Rectangle(110,125,10,30));
+        //fila6
+        nivel2Hitboxes.add(new Rectangle(460,155,10,30));
+        nivel2Hitboxes.add(new Rectangle(340,155,10,30));
+        //fila7
+        nivel2Hitboxes.add(new Rectangle(190,185,10,30));
+        nivel2Hitboxes.add(new Rectangle(490,185,10,30));
+        //fila8
+        nivel2Hitboxes.add(new Rectangle(460,215,10,30));
+        nivel2Hitboxes.add(new Rectangle(100,215,10,30));
+        nivel2Hitboxes.add(new Rectangle(260,215,10,30));
+
+        //fila9
+        nivel2Hitboxes.add(new Rectangle(210,245,10,30));
+        nivel2Hitboxes.add(new Rectangle(480,245,10,30));
+
+
 
 
         JPanel nivel2 = new JPanel() {
             @Override
-            public void paintComponent(Graphics g) {
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2d = (Graphics2D) g;
 
+                g2d.setColor(Color.RED);
+                g2d.fillRect(60, 270, 60, 60);
+                g.setColor(Color.BLACK);
+
+
+                for (Rectangle rect : nivel2Hitboxes) {
+                    g.fillRect(rect.x, rect.y, rect.width, rect.height);
+                }
             }
         };
+
 
         JPanel nivel1 = new JPanel() {
             @Override
@@ -257,6 +403,7 @@ public class Main extends JFrame {
                     contentPane.repaint();
 
                     nivel2.add(jugador);
+
                     nivel1Hitboxes.clear();
 
 
