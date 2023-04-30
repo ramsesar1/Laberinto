8584e74b1e4e7c84cdbc72183504fbbcd14bbe0b
ramsesar1 <ramsesa_19@alu.uabcs.mx>
2023-04-29 22:10:38 -0700

    Se finaliza el diseño del primer nivel, añade la meta (aun no funciona), y se cierra el spawn para que el jugador no se salga del juego.

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -37,53 +37,143 @@ public class Main extends JFrame {
 
         //hitboxes para nivel 1
         ArrayList<Rectangle> hitbox = new ArrayList<>();
+
         hitbox.add(new Rectangle(30, 30, 10, 280));
+
+
+        //-----------------------hitboxes del nivel1----------------------------------------
+
+        //----------filas------------
+
+        //fila 1
         hitbox.add(new Rectangle(570, 0, 10, 280));
+
         hitbox.add(new Rectangle(30, 35, 520, 10));
+        hitbox.add(new Rectangle(30, 0, 550, 10));
+        hitbox.add(new Rectangle(30, 0, 10, 60));
 
-        hitbox.add(new Rectangle(280, 65, 250, 10));
-        hitbox.add(new Rectangle(60, 65, 250, 10));
 
 
-        hitbox.add(new Rectangle(30, 95, 520, 10));
-        hitbox.add(new Rectangle(60, 125, 550, 10));
-        hitbox.add(new Rectangle(30, 155, 520, 10));
-        hitbox.add(new Rectangle(60, 185, 550, 10));
-        hitbox.add(new Rectangle(30, 215, 520, 10));
-        hitbox.add(new Rectangle(60, 245, 520, 10));
+        //fila2
+        hitbox.add(new Rectangle(280, 65, 360, 10));
+        hitbox.add(new Rectangle(60, 65, 190, 10));
+
+        //fila3
+        hitbox.add(new Rectangle(280, 95, 80, 10));
+        hitbox.add(new Rectangle(30, 95, 180, 10));
+
+
+        //fila4
+        hitbox.add(new Rectangle(60, 125, 250, 10));
+        hitbox.add(new Rectangle(350, 125, 140, 10));
+
+        //fila5
+        hitbox.add(new Rectangle(30, 155, 120, 10));
+        hitbox.add(new Rectangle(250, 155, 40, 10));
+        hitbox.add(new Rectangle(370, 155, 120, 10));
+
+
+        //fila6
+        hitbox.add(new Rectangle(60, 185, 90, 10));
+        hitbox.add(new Rectangle(120, 185, 250, 10));
+        hitbox.add(new Rectangle(420, 185, 120, 10));
+
+
+        //fila7
+        hitbox.add(new Rectangle(60, 215, 120, 10));
+        hitbox.add(new Rectangle(220, 215, 150, 10));
+        hitbox.add(new Rectangle(480, 215, 200, 10));
+
+
+        //fila8
+        hitbox.add(new Rectangle(60, 245, 100, 10));
+        hitbox.add(new Rectangle(180, 245, 80, 10));
+        hitbox.add(new Rectangle(380, 245, 140, 10));
+
+
+        //fila9
         hitbox.add(new Rectangle(60, 270, 520, 10));
+
+
+        //----------------columnas--------------------
+
+        //fila1
+        hitbox.add(new Rectangle(120,35,10,30));
+        hitbox.add(new Rectangle(150,35,10,30));
+
+
+        //fila2
+
+        hitbox.add(new Rectangle(240,65,10,30));
+        hitbox.add(new Rectangle(350,65,10,30));
+        hitbox.add(new Rectangle(410,65,10,30));
+
+        //fila3
+        hitbox.add(new Rectangle(280,95,10,30));
+        hitbox.add(new Rectangle(350,95,10,30));
+        hitbox.add(new Rectangle(480,95,10,30));
+
+
+        //fila4
+        hitbox.add(new Rectangle(180,125,10,30));
+        hitbox.add(new Rectangle(510,125,10,30));
+
+
+        //fila5
+        hitbox.add(new Rectangle(80,155,10,30));
+        hitbox.add(new Rectangle(240,155,10,30));
+        hitbox.add(new Rectangle(360,155,10,30));
+        hitbox.add(new Rectangle(140,155,10,30));
+        hitbox.add(new Rectangle(510,155,10,30));
+
+
+        //fila6
+        hitbox.add(new Rectangle(510,185,10,30));
+        hitbox.add(new Rectangle(450,185,10,30));
+        hitbox.add(new Rectangle(360,185,10,30));
+
+
+        //fila7
+        hitbox.add(new Rectangle(510,215,10,30));
+        hitbox.add(new Rectangle(350,215,10,30));
+
+        hitbox.add(new Rectangle(60,215,10,30));
+
+
+        //fila8
+        hitbox.add(new Rectangle(60,245,10,30));
+        hitbox.add(new Rectangle(180,245,10,30));
+        hitbox.add(new Rectangle(120,245,10,30));
+
+        //meta
+
+
+
+        //-----------------------hitboxes del nivel2----------------------------------------
+
+
 
         JPanel nivel1 = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
+                Graphics2D g2d = (Graphics2D) g;
+
+                g2d.setColor(Color.RED);
+                g2d.fillRect(60, 270, 60, 60);
                 g.setColor(Color.BLACK);
-                /*
-                g.fillRect(30, 0, 550, 15);
-                g.fillRect(30, 30, 15, 280);
-                g.fillRect(560, 0, 15, 280);
-
-                //filas
-                g.fillRect(30, 30, 520, 15);
-                g.fillRect(60, 60, 550, 15);
-                g.fillRect(30, 90, 520, 15);
-                g.fillRect(60, 120, 550, 15);
-                g.fillRect(30, 150, 500, 15);
-                g.fillRect(60, 180, 550, 15);
-                g.fillRect(30, 210, 520, 15);
-                g.fillRect(30, 240, 520, 15);
-                g.fillRect(60, 270, 520, 15);
-
-*/
 
 
-                // inserta las hitbox en el panel
-              //  g.setColor(Color.RED);
                 for (Rectangle rect : hitbox) {
                     g.fillRect(rect.x, rect.y, rect.width, rect.height);
                 }
             }
         };
+
+        JPanel nivel2 = new JPanel(){
+
+        };
+
 
         JButton jugador = new JButton();
         jugador.setBackground(Color.GREEN);
